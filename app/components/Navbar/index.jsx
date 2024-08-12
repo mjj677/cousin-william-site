4e470b1f94544f7934dd5d32707a502a55a5db83
Matt Johnston <mattjohnston2000@gmail.com>
2024-08-12 17:47:58 +0100

    added styling library

---
+++ app/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import * as Styles from "./styles";
+
+export const Navbar = () => {
+  return (
+    <>
+      <Styles.Button>NAVBAR</Styles.Button>
+  </>
+);
+};
--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
 import styles from "./page.module.css";
+import { Navbar } from "./components/Navbar";
 
 export default function Home() {
- return <h1>I LIKE MUMS</h1>
+  return (
+    <>
+      <h1>COUSIN WILLIAM</h1>
+      <Navbar />
+    </>
+  );
 }
